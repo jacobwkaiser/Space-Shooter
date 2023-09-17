64a44a2561291de856892c67af107d45752e82b6
jacobwkaiser <kaiserjacob639@gmail.com>
2023-09-17 18:09:36 -0400

    Complete P2

--- player/Player.gd
+++ player/Player.gd
@@ -3,17 +3,36 @@ extends CharacterBody2D
 var speed = 5
 var max_speed = 400
 var rotate_speed = 0.08
+var nose = Vector2(0,-60)
+var Bullet = load("res://bullet.tscn")
 
-
-func _physics_process(delta):
+func get_input():
+	var to_return = Vector2.ZERO
+	$Exhaust.hide()
+	$Exhaust2.hide()
+	if Input.is_action_pressed("Forward"):
+		to_return += Vector2(0,-1)
+		$Exhaust.show()
+		$Exhaust2.show()
 	if Input.is_action_pressed("Left"):
-		rotation = rotation - rotate_speed
+		rotation -= rotate_speed
 	if Input.is_action_pressed("Right"):
-		rotation = rotation + rotate_speed
-	if Input.is_action_pressed("Foward"):
-		velocity = velocity + Vector2(0,-speed).rotated(rotation)
+		rotation += rotate_speed
+	return to_return.rotated(rotation) 
+
+func _physics_process(_delta):
+	velocity += get_input()*speed
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
 	
 	position.x = wrapf(position.x, 0, 1152)
 	position.y = wrapf(position.y, 0, 648)
 	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
 	move_and_slide()
+
+	if Input.is_action_just_pressed("Shoot"):
+		var Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var bullet = Bullet.instantiate()
+			bullet.rotation = rotation
+			bullet.global_position = global_position + nose.rotated(rotation)
+			Effects.add_child(bullet)
