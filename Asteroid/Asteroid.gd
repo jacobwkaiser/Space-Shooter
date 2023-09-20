827630e26350e5a88aa0e9109a922cd9b83a9cad
jacobwkaiser <kaiserjacob639@gmail.com>
2023-09-20 18:41:06 -0400

    Complete Assignment

--- Asteroid/Asteroid.gd
+++ Asteroid/Asteroid.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 
 var initial_speed = 105.0
+var health = 5
 
 func _ready():
 	velocity = Vector2(0,initial_speed*randf()).rotated(2*PI*randf())
@@ -11,3 +12,9 @@ func _physics_process(_delta):
 	move_and_slide()
 	position.x = wrapf(position.x, 0, 1152)
 	position.y = wrapf(position.y, 0, 648)
+
+
+func damage(d):
+	health -=  d
+	if health <= 0:
+		queue_free()
---
+++ Effects/Explosion.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+func _ready():
+	play("default")
+
+
+
+
+
+func _on_animation_finished():
+	queue_free()
