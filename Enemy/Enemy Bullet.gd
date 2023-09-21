b4edfe5c21d4af7a6c1ae4302a48eca98f6f6900
jacobwkaiser <kaiserjacob639@gmail.com>
2023-09-21 10:25:30 -0400

    update complete

--- Enemy/Enemy Bullet.gd
+++ Enemy/Enemy Bullet.gd
@@ -20,7 +20,7 @@ func _physics_process(_delta):
 func _on_body_entered(body):
 	if body.has_method("damage"):
 		body.damage(damage)
-	Effects = get_node_or_null("root/Game/Effects")
+	Effects = get_node_or_null("/root/Game/Effects")
 	if Effects != null:
 		var explosion = Explosion.instantiate()
 		Effects.add_child(explosion)
